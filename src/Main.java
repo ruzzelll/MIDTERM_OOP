7a7ba16d8559ba809f66511d1d37998aa3f502b9
jetoro <76423923+pl0wox@users.noreply.github.com>
2022-10-07 09:33:09 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+
 public class Main {
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println("STUDENT 1");
+        Student jeth = new Student();
+
+        jeth.FName = "Jethro";
+
+        System.out.println(jeth.FName);
 
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,4 @@
+public class Person {
+    int ID, PNumber;
+    String FName, LName, Gender, Address;
+}
